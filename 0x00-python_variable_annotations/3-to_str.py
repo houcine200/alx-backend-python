40304b9f02bb58782c4f94b6bf9d7274c9d408cf
Houcine Walaq <houcine.walaq@gmai.com>
2024-04-18 13:22:23 +0100

    3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Contains a type-annotated function to convert a float
+ to its string representation."""
+
+
+def to_str(n: float) -> str:
+    """Converts a float to its string representation."""
+    return str(n)
