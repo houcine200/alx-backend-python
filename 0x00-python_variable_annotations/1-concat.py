721d673c0f61dfbff5d209a251528cb45ebc6540
Houcine Walaq <houcine.walaq@gmai.com>
2024-04-18 12:59:50 +0100

    1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """Contains a function to concatenate two strings."""
+
+
 def concat(str1: str, str2: str) -> str:
     """Concatenate two strings"""
     return str1 + str2
