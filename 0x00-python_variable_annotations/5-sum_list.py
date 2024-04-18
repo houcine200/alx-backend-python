3f532723f3000e783b79b7fc39bcd95a3f1b7289
Houcine Walaq <houcine.walaq@gmai.com>
2024-04-18 14:24:11 +0100

    5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Contains a function to compute the sum of a list of floats."""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Computes the sum of a list of floats."""
+    return sum(input_list)
