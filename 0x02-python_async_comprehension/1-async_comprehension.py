79b1057d85adcd4a9b34aa26d261d1018e3eec43
Houcine Walaq <houcine.walaq@gmai.com>
2024-04-23 16:02:05 +0100

    1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -11,7 +11,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """Return the 10 random numbers"""
-    results = []
-    async for i in async_generator():
-        results.append(i)
+    results = [i async for i in async_generator()]
     return results
