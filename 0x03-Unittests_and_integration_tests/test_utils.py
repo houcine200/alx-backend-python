30fb73b582757a428937776ef4a17bc996eab5f8
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-10 19:08:42 +0100

    task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,3 +19,11 @@ class TestAccessNestedMap(unittest.TestCase):
     ) -> None:
         """test access_nested_map function"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([({}, ("a",)), ({"a": 1}, ("a", "b"))])
+    def test_access_nested_map_exception(
+        self, nested_map: Dict[str, Any], path: Tuple[str]
+    ) -> None:
+        """test access_nested_map function for exceptions"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
