0e2d70f673eae71e53a516ed5ba00a7839baf178
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-11 18:30:21 +0100

    3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,54 +1,50 @@
 #!/usr/bin/env python3
-""" doc doc doc """
+"""
+Parameterize a unit test
+"""
 import unittest
-from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
+from parameterized import parameterized
 from typing import Any, Tuple, Dict
 from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """doc doc doc"""
-
-    @parameterized.expand(
-        [
+    @parameterized.expand([
             ({"a": 1}, ("a",), 1),
             ({"a": {"b": 2}}, ("a",), {"b": 2}),
             ({"a": {"b": 2}}, ("a", "b"), 2),
-        ]
-    )
+        ])
     def test_access_nested_map(
         self, nested_map: Dict[str, Any], path: Tuple[str], expected: Any
     ) -> None:
-        """doc doc doc"""
+        """test access_nested_map function"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([({}, ("a",)), ({"a": 1}, ("a", "b"))])
     def test_access_nested_map_exception(
         self, nested_map: Dict[str, Any], path: Tuple[str]
     ) -> None:
-        """doc doc doc"""
+        """test access_nested_map function for exceptions"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
 
 
 class TestGetJson(unittest.TestCase):
-    """doc doc doc"""
-
+    """mock requests"""
     @parameterized.expand(
         [
             ("http://example.com", {"payload": True}),
             ("http://holberton.io", {"payload": False}),
         ]
     )
-    @patch("requests.get")
-    def test_get_json(
-        self, test_url: str, test_payload: Dict[str, Any], mock_get: Mock
-    ) -> None:
-        """doc doc doc"""
+    @patch('requests.get')
+    def test_get_json(self,  test_url, test_payload, mock_get):
+        """ test that utils.get_json returns the expected result. """
         mock_get.return_value.json.return_value = test_payload
-        self.assertEqual(get_json(test_url), test_payload)
+        result = get_json(test_url)
         mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
 
 
 class TestMemoize(unittest.TestCase):
