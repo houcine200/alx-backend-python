27a9344902a4a2f15f7045b588679b82c0bcf827
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-11 18:18:20 +0100

    3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -64,9 +64,8 @@ class TestMemoize(unittest.TestCase):
                 """doc doc doc"""
                 return self.a_method()
 
-        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
-            inst = TestClass()
-            inst.a_property
-            inst.a_property
-
-            mock_method.assert_called_once()
+        with patch.object(TestClass, "a_method", return_value=42) as mocked:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property, 42)
+            self.assertEqual(test_class.a_property, 42)
+            mocked.assert_called_once()
