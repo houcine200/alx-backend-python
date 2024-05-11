d0c92d00b43109c58531e19853f8d0b4ae6e59df
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-11 18:22:43 +0100

    3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -49,25 +49,34 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
-    """doc doc doc"""
+    """Test Memoize"""
 
-    def test_memoize(self) -> None:
-        """doc doc doc"""
-
+    def test_memoize(self):
+        """test memoize decorator"""
         class TestClass:
-            """doc doc doc"""
+            """
+            Test class
+            """
+            def __init__(self) -> None:
+                """
+                initlaize instance attributes
+                """
+                self.a_method_call = 0
 
-            def a_method(self) -> int:
-                """doc doc doc"""
+            def a_method(self):
+                """targeted method"""
+                self.a_method_call += 1
                 return 42
 
             @memoize
-            def a_property(self) -> int:
-                """doc doc doc"""
+            def a_property(self):
+                """memoize method"""
                 return self.a_method()
 
-        with patch.object(TestClass, "a_method", return_value=42) as mocked:
-            test_class = TestClass()
-            self.assertEqual(test_class.a_property, 42)
-            self.assertEqual(test_class.a_property, 42)
-            mocked.assert_called_once()
+        test_obj = TestClass()
+        call_method_1 = test_obj.a_property
+        call_method_2 = test_obj.a_property
+
+        self.assertEqual(call_method_1, call_method_2)
+        self.assertEqual(call_method_1, 42)
+        self.assertEqual(test_obj.a_method_call, 1)
