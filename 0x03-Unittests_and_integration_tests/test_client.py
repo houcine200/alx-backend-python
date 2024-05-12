ada84e692d9f782410ba183270aebd36c24d2de6
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-12 14:47:52 +0100

    DONE

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -20,8 +20,8 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch("client.get_json", return_value={"payload": True})
     def test_org(self, org_name: str, mock_get: Mock) -> None:
         """Test the org method."""
-        github_org_client = GithubOrgClient(org_name)
-        self.assertEqual(github_org_client.org, {"payload": True})
+        org_client = GithubOrgClient(org_name)
+        self.assertEqual(org_client.org, {"payload": True})
         url = f"https://api.github.com/orgs/{org_name}"
         mock_get.assert_called_once_with(url)
 
