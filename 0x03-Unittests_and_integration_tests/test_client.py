845a5f1cdbcda2a79c204d92c48d69b788b06d27
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-12 14:45:29 +0100

    3

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,21 +1,17 @@
-#!/usr/bin/env python3
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
 from client import GithubOrgClient
 
-
 class TestGithubOrgClient(unittest.TestCase):
-    """_class and implement the test_org method.
-    """
+
     @parameterized.expand([
-        ('google'),
-        ('abc')
+        ("google",),
+        ("abc",),
     ])
     @patch('client.get_json')
     def test_org(self, org_name: str, mock_get: Mock) -> None:
-        """ test that GithubOrgClient.org returns the correct value."""
         client = GithubOrgClient(org_name)
-        self.assertEqual(client.org, {"payload": True})
+        client.org()
         url = f"https://api.github.com/orgs/{org_name}"
         mock_get.assert_called_once_with(url)
